0d7c81be94dd350a93266005ff37144fa3ec3a64
ehtbanton <notnatheyam@gmail.com>
2024-10-19 14:34:09 +0100

    read CSV file

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -76,7 +76,8 @@ class Mission:
 
     @classmethod
     def from_csv(cls, file_name: str):
-        # You are required to implement this method
+        # Read the CSV file using pandas
+        df = pd.read_csv(file_name)
         pass
 
 

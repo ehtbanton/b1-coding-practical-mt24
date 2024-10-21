08d6314d4beb9efe63a595e86eb8418d5c3f694c
ehtbanton <notnatheyam@gmail.com>
2024-10-21 17:48:04 +0100

    added controller

---
+++ uuv_mission/controller.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class PDController:
+    def __init__(self, Kp: float = 0.15, Kd: float = 0.6):
+        self.Kp = Kp
+        self.Kd = Kd
+        self.previous_error = 0
+
+    def compute(self, reference: float, measurement: float) -> float:
+        error = reference - measurement
+        delta_error = error - self.previous_error
+        
+        control_action = self.Kp * error + self.Kd * delta_error
+        
+        self.previous_error = error
+        
+        return control_action
